9100b3c13f1fb16704749382f8ee9c6450964e24
Daniel C.H <dchernandez27@outlook.com>
2024-05-28 20:24:36 +0200

    no images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,5 @@ import { KlCarouselComponent } from '../../dist/ngx-kl-carousel';
 export class AppComponent {
   title = 'kl-carousel';
   images=[
-"https://res.cloudinary.com/dzlu0ltrg/image/upload/w_400/q_auto:best/v1701116932/Moky/title_2_z1y9jy.jpg",
-"https://res.cloudinary.com/dzlu0ltrg/image/upload/w_400/q_auto:best/v1701116936/Moky/title_kjlbdb.jpg",
-"https://res.cloudinary.com/dzlu0ltrg/image/upload/w_400/q_auto:best/v1701116936/Moky/title_kjlbdb.jpg",
-"https://res.cloudinary.com/dzlu0ltrg/image/upload/w_400/q_auto:best/v1701116936/Moky/title_kjlbdb.jpg",
-"https://res.cloudinary.com/dzlu0ltrg/image/upload/w_400/q_auto:best/v1701116552/Moky/title_slk9vk.jpg"
   ]
 }
